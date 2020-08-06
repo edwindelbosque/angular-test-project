1bb3e6e0e9de7369550563d3780d3bfac80f6cbb
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-08-06 15:51:30 -0600

    Add logic and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  passwordLength = 0;
+
+  setPasswordLength = (value: number) => {
+    this.passwordLength = value;
+  };
+
+  toggleLetters = () => {
+    this.includeLetters = !this.includeLetters;
+  };
+
+  toggleNumbers = () => {
+    this.includeNumbers = !this.includeNumbers;
+  };
+
+  toggleSymbols = () => {
+    this.includeSymbols = !this.includeSymbols;
+  };
+
+  onButtonClick = () => {
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+
+    let validCharacters = '';
+
+    if (this.includeLetters) {
+      validCharacters += letters;
+    }
+
+    if (this.includeNumbers) {
+      validCharacters += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validCharacters += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validCharacters.length);
+      generatedPassword += validCharacters[index];
+    }
+
+    this.password = generatedPassword;
+  };
 }
